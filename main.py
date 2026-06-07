e0fd24d4d67183cef7a1deaa0c6d2e2b45fd23f8
Екатерина <ev-poltorak@mail.ru>
2026-06-07 21:17:15 +1000

    Выбрала новую ллм Квен и отключила ей рассуждения вслух

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ from groq import Groq
 client = Groq(api_key="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")
 #функция с промтом для нейросети
 def summarize_news(title, content):
-    prompt = f"Сделай краткое содержание новости (1 предложение, максимум 25 слов) на русском. Используй информацию из каждого предложения исходной новости, так чтобы не было похоже на заголовок, но общий его смысл сохранялся:\nЗаголовок: {title}\nТекст: {content}"
+    prompt = f"Сделай краткое содержание новости (1 предложение, максимум 10 слов) на русском. Используй информацию из каждого предложения исходной новости, так чтобы не было похоже на заголовок, но общий его смысл сохранялся. Ответь одним предложением без размышлений:\nЗаголовок: {title}\nТекст: {content}"
     try:
         completion = client.chat.completions.create(
-            model="llama-3.3-70b-versatile",  #доступная и бесплатная модель от Meta
+            model="qwen/qwen3-32b",  #доступная и бесплатная модель от Qwen
             messages=[
                 {"role": "user", "content": prompt}
             ],
             temperature=0.3, #умеренная креативность модели
             max_tokens=300, #длина ответов - максимум 300 токенов
             top_p=1, #параметр разнообразия слов
+            reasoning_effort="none" #отключение рассуждений у модели
         )
         return completion.choices[0].message.content.strip()
 
